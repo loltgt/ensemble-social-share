c09c3539916fde23eecfffe774d31aacaab03ba1
Leonardo Laureti <204957+leolweb@users.noreply.github.com>
2025-02-19 23:25:41 +0100

    icons and script to generate

---
+++ src/icons/BUILD.md
@@ -0,0 +1,81 @@
+# Build instructions
+
+Follow these instructions to replicate the default icon set used by ensemble SocialShare.
+
+> [!TIP]
+> You can create your own icon set, create a new npm package with your settings
+> with `npm init` command, or rely to the existing "package.json" as starting point
+
+
+## Get started
+
+First download "Ionicons" and "Simple Icons" tarballs, using npm:
+```shell
+npm install
+```
+
+To copy the default icons using predefined list with the command:
+```shell
+npm run copy
+```
+
+To add single icons to your project:
+```shell
+npm run copy.ionicons icon-name
+npm run copy.simple-icons icon-name
+```
+
+Icons will be copied to "project" folder by default.
+
+To select another folder, simply pass the folder name as a secundary argument:
+```shell
+npm run copy folder-name
+```
+
+
+## Use SVG symbols
+
+If you prefer SVG symbols instead of web font, set SocialShare options to `icons: 'symbols'`
+```javascript
+new ensemble.SocialShare(element,
+  {  
+    icons: 'symbols'
+  }
+);
+)
+```
+
+Copy all `*.svg` files from your "project" folder.
+
+
+## Use a Webfont
+
+To create a web font I used [IcoMoon.app](https://icomoon.app)
+
+Create an new empty project and *Import Set*
+
+Select the previously copied icons from your "project" folder.
+
+Set the Grid size to: 32
+
+> [!IMPORTANT]
+> Do not forget to select a proper license for your webfont and enter the metadata in *Properties*
+
+*Select All* the icons and press *Generate Font*, check that everything is in order.
+
+Edit exporting preferences by clicking on the cog icon, next to the *Download* button.
+
+Finally press *Download* to get your generated font.
+
+
+## Export the Webfont to WOFF2 Webfont format
+
+To create WOFF2 web font I used [Font Squirrel Webfont Generator](https://www.fontsquirrel.com/tools/webfont-generator)
+
+> [!TIP]
+> You can upload the file "generator_config.txt" to use the same preset I used
+
+Upload the previously generated font, select `*.ttf` font and *Upload*
+
+Wait until the font is generated and finally press *Download* to get your generated font.
+
---
+++ src/icons/copy.js
@@ -0,0 +1,63 @@
+
+import { argv, exit } from 'node:process';
+import { copyFile, mkdir, access } from 'node:fs/promises';
+
+const BASE_PATH = './node_modules';
+const DEST_PATH = './project';
+
+const IONICONS_PATH = `${BASE_PATH}/ionicons/src/svg`;
+const SIMPLEICONS_PATH = `${BASE_PATH}/simple-icons/icons`;
+
+const ICONS_DEFAULT = {
+  ionicons: [{'share-social': 'web-share'}, {'link-outline': 'copy-link'}, {'mail-outline': 'send-email'}, 'linkedin', 'skype', 'stumbleupon'],
+  simpleicons: ['bluesky', 'facebook', 'line', 'mastodon', 'messenger', 'quora', 'reddit', 'snapchat', 'telegram', 'threads', 'viber', 'whatsapp', 'x']
+};
+
+
+try {
+  try {
+    await access(argv[4] ?? DEST_PATH);
+  } catch (err) {
+    if (err.code === 'ENOENT')
+      await mkdir(argv[4] ?? DEST_PATH);
+    else
+      throw err;
+  }
+  console.log('folder', argv[4] ?? DEST_PATH);
+
+  await access(IONICONS_PATH);
+  await access(SIMPLEICONS_PATH);
+} catch (err) {
+  throw err;
+}
+
+switch (argv[2]) {
+  case 'ionicons':
+    copyFile(`${IONICONS_PATH}/${argv[3]}.svg`, argv[4] ?? DEST_PATH)
+      .then(() => console.log('copied', argv[4] ?? DEST_PATH, `${IONICONS_PATH}/${argv[3]}.svg`))
+      .catch(console.error);
+  break;
+  case 'simple-icons':
+    copyFile(`${SIMPLEICONS_PATH}/${argv[3]}.svg`, argv[4] ?? DEST_PATH)
+      .then(() => { console.log('copied', argv[4] ?? DEST_PATH, `${IONICONS_PATH}/${argv[3]}.svg`) })
+      .catch(console.error);
+  break;
+  case '--default':
+    for (const iconset in ICONS_DEFAULT) {
+      const src_path = {ionicons: IONICONS_PATH, simpleicons: SIMPLEICONS_PATH}[iconset];
+
+      for (const icon of ICONS_DEFAULT[iconset]) {
+        let src = icon, dest = icon;
+
+        if (typeof icon == 'object')
+          [src, dest] = Object.entries(icon).flat();
+
+        copyFile(`${src_path}/${src}.svg`, argv[4] ? `${argv[4]}/${dest}.svg` : `${DEST_PATH}/${dest}.svg`)
+          .then(() => { console.log('copied', argv[4] ? `${argv[4]}/${dest}.svg` : `${DEST_PATH}/${dest}.svg`, `${src_path}/${src}.svg`) })
+          .catch(console.error)
+      }
+    }
+  break;
+  default:
+    exit(1);
+}
